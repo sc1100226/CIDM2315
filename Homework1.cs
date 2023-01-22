f67c561a526afb1f69a691b86be1b307f6b4d176
sc1100226 <123280333+sc1100226@users.noreply.github.com>
2023-01-22 00:04:09 -0600

    Upload Homework Week1 part 1

---
+++ Homework1.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        double x = 2.5;
+        double y = 3.3;
+        double z;
+
+        Console.WriteLine($"x = {x}, y = {y}.");
+
+        z = (4*x*x + 3*y);
+
+        Console.WriteLine($"The value of Z is : {z}.");
+        
+    }
+}
