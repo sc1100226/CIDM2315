9b7064ac92cfe818de6d571cbaa3ec21276265ff
sc1100226 <123280333+sc1100226@users.noreply.github.com>
2023-02-13 21:24:48 -0600

    Add files via upload

---
+++ Week4HW4Q1.cs
@@ -0,0 +1,21 @@
+﻿namespace Week4Homeowrk4Q1;
+class Program
+{
+    static void Main(string[] args)
+    {
+       int a = 3;
+        int b = 5;
+         int numbers = greaternum(a,b);
+
+        Console.WriteLine($"a = {a}; b={b}");
+        Console.WriteLine($"The largest number is: {numbers} ");
+
+
+    }
+    public static int greaternum(int a, int b){
+      if (a > b)
+        return a;
+      else
+        return b; 
+    }
+}
---
+++ Week4HW4Q2.cs
@@ -0,0 +1,43 @@
+﻿namespace Week4Homework4Q2;
+class Program
+{
+    
+   static void pattern(int N,string shape){
+    
+    int row,col;
+    if(shape == "left"){
+    
+       for(row = 1;row <= N;row++)
+       {
+        for(col = 1;col <= row;col++)
+           Console.Write("*");
+        Console.Write("\n");
+       }
+      }
+    else if(shape=="right"){
+    
+     for(row = 1; row <= N; row++){
+      for(col = N; col >= 1; col--){
+        if(row < col)
+          Console.Write(" ");
+        else  
+          Console.Write("*");
+        }
+      Console.WriteLine();
+    }  
+        Console.WriteLine();}
+    }
+    public static void Main() 
+{
+   int N;
+   string shape;
+   Console.Write("Enter a number: ");
+   N = Convert.ToInt32(Console.ReadLine());
+   Console.Write("Enter left or right: ");
+   shape = Console.ReadLine();
+   Console.WriteLine(" ");
+   Console.Write("N is: "+N+"; shape is "+shape+"\n");
+   pattern(N,shape);
+}    
+    
+}
